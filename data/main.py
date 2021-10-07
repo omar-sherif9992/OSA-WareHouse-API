9cbe119a7b764f9e7cccc3e33e86e0bc2482b823
Omar Sherif Ali <69806823+omar-sherif9992@users.noreply.github.com>
2021-10-07 23:35:28 +0200

    Update main.py

--- data/main.py
+++ data/main.py
@@ -13,14 +13,14 @@ from uploader import Uploader
 
 
 
-if __name__=="__main__":
-    try:
-        jumia = Jumia()
-        jumia_uploader=Uploader(user=jumia,user_products=jumia.all_products_info)
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+# if __name__=="__main__":
+#     try:
+#         jumia = Jumia()
+#         jumia_uploader=Uploader(user=jumia,user_products=jumia.all_products_info)
+#     except KeyboardInterrupt:
+#         print('Interrupted')
+#         try:
+#             sys.exit(0)
+#         except SystemExit:
+#             os._exit(0)
 
